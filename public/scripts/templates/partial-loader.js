563e8937d00422ce14cd490fe98f542ecbe889ff
doubleshot1 <nenost12@gmail.com>
2016-09-28 11:53:01 +0300

    Fix templates-cache

--- public/scripts/templates/partial-loader.js
+++ public/scripts/templates/partial-loader.js
@@ -1,27 +1,27 @@
 'use strict'
 import 'jquery'
-const partialLoader = (function (){
+const partialLoader = (function () {
     const cache = {};
-    
-    function get (partialName){
 
-        return new Promise((resolve, reject)=>{
-            if (cache[partialName]){
+    function get(partialName) {
+
+        return new Promise((resolve, reject) => {
+            if (cache[partialName]) {
                 resolve(cache[partialName]);
+            } else {
+                $.get(`/scripts/templates/partials/${partialName}.hb`)
+                    .done((data) => {
+                        let partial = Handlebars.compile(data);
+                        cache[partialName] = partial;
+                        resolve(partial);
+                    });
             }
-
-            $.get(`/scripts/templates/partials/${partialName}.hb`)
-            .done((data) => {
-                let partial = Handlebars.compile(data);
-                cache[partialName] = partial;
-                resolve(partial);
-            });
         });
     }
-    
+
     return {
         get
     };
-}());
+} ());
 
 export {partialLoader};
