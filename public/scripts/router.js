0ad331761b21808396a3b88b9277128ffd861613
Milena Stancheva <mil.stancheva@gmail.com>
2016-09-30 18:06:01 +0300

    index.html updated facebook.handlebars bootsrtap in progress logo image - demo added content conteiner in progress

--- public/scripts/router.js
+++ public/scripts/router.js
@@ -1,18 +1,34 @@
-const router = (function (){
+const router = (function () {
     let navigo;
-    
-    function init(){
-        navigo = new Navigo(null, false);
 
-        navigo.on ('/index', function () {
-            console.log('loaded');
-        }).resolve();
-        
+    function init() {
+        navigo = new Navigo(null, true);
+
+        navigo
+            .on('/index', function () {
+                console.log('loaded');
+            })
+            .on('#/facebook-login', () => {
+
+            })
+            .on('#/twitter-login', () => {
+
+            })
+            .on('#/instagram-login', () => {
+
+            })
+            .on('#/about', () => {
+
+            })
+            .on('#/logout', () => {
+
+            })
+            .resolve();
     }
 
     return {
         init
     }
-}());
+} ());
 
 export {router}
