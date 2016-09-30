79d3c998ef2928c5c8f92c34c0c94d66d430b603
nkolchakov <nikola.kolchakov@gmail.com>
2016-09-30 18:35:19 +0300

    add partial Instagram

--- public/scripts/router.js
+++ public/scripts/router.js
@@ -1,34 +1,49 @@
-const router = (function () {
+const router = (function() {
     let navigo;
 
     function init() {
-        navigo = new Navigo(null, true);
+        navigo = new Navigo(null, false);
 
         navigo
-            .on('/index', function () {
+            .on('/index', function() {
                 console.log('loaded');
             })
-            .on('#/facebook-login', () => {
+            .on('/instagram', () => {
 
-            })
-            .on('#/twitter-login', () => {
 
-            })
-            .on('#/instagram-login', () => {
+                var feed = new Instafeed({
+                    get: 'tagged',
+                    tagName: 'gaming',
+                    userID: ' 3008598822',
+                    accessToken: '3008598822.ba4c844.379aabfdd8214190868c0f2aa93350ab',
+                    success: (data) => {
+                        templateLoader.get('instagram')
+                            .then(funcTemplate => {
+                                let html = funcTemplate(data);
+                                $('#content').html(html);
+                            })
+                    }
 
-            })
-            .on('#/about', () => {
 
+                });
+                feed.run();
             })
-            .on('#/logout', () => {
+            .on('/facebook', () => {
+                templateLoader.get('facebook')
+                    .then(funcTemplate => {
+                        let html = funcTemplate();
+                        $('#content').html(html);
+                    })
+            })
 
-            })
-            .resolve();
+        .resolve();
+
+
     }
 
     return {
         init
     }
-} ());
+}());
 
-export {router}
+export { router }
